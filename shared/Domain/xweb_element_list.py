7d34b0461ee6dddd2ef2602828fa280f48d51974
masaya_nishigaki <masa199311266@gmail.com>
2022-02-14 11:59:28 +0900

    Revert "add scraping method"

--- shared/Domain/xweb_element_list.py
+++ shared/Domain/xweb_element_list.py
@@ -1,23 +1,11 @@
 from typing import List
 from shared.Domain.xweb_element import XWebElement
-from functools import total_ordering
 
 
-@total_ordering
 class XWebElementList:
     def __init__(self, web_elements: List[XWebElement]):
         self.web_element_list = []
         list(map(lambda web_element: self.add(web_element), web_elements))
-
-    def __eq__(self, other):
-        if not isinstance(other, XWebElementList):
-            return NotImplemented
-        return (self.web_element_list) == (other.web_element_list)
-
-    def __lt__(self, other):
-        if not isinstance(other, XWebElementList):
-            return NotImplemented
-        return (self.web_element_list) < (other.web_element_list)
 
     def add(self, web_element):
         self.web_element_list.append(web_element)
@@ -26,4 +14,4 @@ class XWebElementList:
         return self.web_element_list
 
     def count(self):
-        return len(self.get_web_element_list())
+        return len(self.web_elements)
--- shared/Enums/SiteType.py
+++ shared/Enums/SiteType.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 class SiteType(Enum):
-    NOT_NEEDS_LOGIN = 1
-    NEEDS_LOGIN_AND_ONE_STEP = 2
-    NEEDS_LOGIN_AND_TWO_STEP = 3
+    NOT_NEEDS_LOGIM = 1
+    NEEDS_LOGIM_AND_ONE_STEP = 2
+    NEEDS_LOGIM_AND_TWO_STEP = 3

db67d0ab287ed08d6a09cf2b90470bfd33ecd1f0
nishigaki <nishigaki@aivick.co.jp>
2022-04-09 18:43:56 +0900

    fix

--- tests/Domain/Twi/test_twi_error_judgement.py
+++ tests/Domain/Twi/test_twi_error_judgement.py
@@ -1,15 +1,6 @@
-import unittest.mock
-from urllib import response
-import pytest
 from packages.twi_automation.Domain.twi_error_handle_judgement import (
     TwiErrorHandleJudgement,
 )
-from packages.twi_automation.env import ENV
-from shared.Domain.Time.x_date_time import XDateTime
-
-from shared.Domain.Twi.twitter_operator import TwitterOperator
-from shared.Domain.xstr import XStr
-import tweepy
 from tweepy import errors
 from requests import Response
 
@@ -17,9 +8,8 @@ from requests import Response
 def test_ツイート_認証エラー() -> None:
     response = Response()
     response.status_code = [401]
-    e = errors.Unauthorized(response)
 
-    # e: errors.TweepyException = errors.TweepyException()
+    e = errors.Unauthorized(response)
     e.api_codes.append(32)
 
     judgement = TwiErrorHandleJudgement(e)

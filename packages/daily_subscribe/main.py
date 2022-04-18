f677c7de6879ddfa59890d222abadef29a7553ab
nishigaki <nishigaki@aivick.co.jp>
2022-04-18 16:10:01 +0900

    fix match case

--- packages/daily_subscribe/main.py
+++ packages/daily_subscribe/main.py
@@ -23,7 +23,9 @@ from shared.x_logger import XLogger
 Initializer().ioOption()
 
 # 自動起動したいサイトのurlやログイン情報を記載したエクセルを読み込み -------------------------------------------------------------------------------
-subscribe_list_filepath = XFileSystemPath(XStr("packages/daily_subscribe/subscribe_list.xlsx")).to_absolute()
+subscribe_list_filepath = XFileSystemPath(
+    XStr("packages/daily_subscribe/subscribe_list.xlsx")
+).to_absolute()
 
 subscribe_wb = XExcel().read(filepath=subscribe_list_filepath, sheet_name=None)
 subscribe_sheet = subscribe_wb.get_sheet_by_name("subscribe_list")
@@ -40,7 +42,7 @@ try:
 except SessionNotCreatedException as e:
     XLogger.exception_to_slack(
         ENV["SLACK_WEBHOOK_URL_JOBCAN"],
-        "webdriverのバージョンがChromeブラウザーのバージョンと一致していないため起動に失敗しました。"
+        "webdriverのバージョンがChromeブラウザーのバージョンと一致していないため起動に失敗しました。",
     )
 
 xdriver = xdriver_factory.create(BrowserType.CHROME, is_headless=False)
@@ -51,51 +53,58 @@ i_web_browser_operator: IWebBrowserOperator = DiContainer().resolve(IWebBrowserO
 # エクセルの内容をもとに、ブラウザ起動
 for subscribe in subscribe_list:
     # ブラウザで起動するサイトの種別に応じてmatch～case文で処理
-    match SiteType(subscribe["site_type"]):
-        case SiteType.NEEDS_LOGIN_AND_TWO_STEP:
-            xbrowser = xbrowser_factory.create(xdriver, XUrl(subscribe["url"]))
-            i_web_browser_operator.boot(xbrowser)
-            
-            sleep(1)
-            user_id = i_web_browser_operator.find_by_id(subscribe["user_id_elem"])
-            
-            i_web_browser_operator.send_value(
-                XWebElementList(
-                    [
-                        user_id.set_value(subscribe["user_id"]),
-                    ]
-                )
+    # →本番のpythonがanacondaの関係で3.9.4のためmatch～case文ではなくif文を使用する
+    if SiteType(subscribe["site_type"]) == SiteType.NEEDS_LOGIN_AND_TWO_STEP:
+        xbrowser = xbrowser_factory.create(xdriver, XUrl(subscribe["url"]))
+        i_web_browser_operator.boot(xbrowser)
+
+        sleep(1)
+        user_id = i_web_browser_operator.find_by_id(subscribe["user_id_elem"])
+
+        i_web_browser_operator.send_value(
+            XWebElementList(
+                [
+                    user_id.set_value(subscribe["user_id"]),
+                ]
             )
+        )
 
-            i_web_browser_operator.find_by_xpath(subscribe["submit_elem"]).web_element().click()
-            sleep(1)
+        i_web_browser_operator.find_by_xpath(
+            subscribe["submit_elem"]
+        ).web_element().click()
+        sleep(1)
 
-            password = i_web_browser_operator.find_by_id(subscribe["pass_elem"])
-            i_web_browser_operator.send_value(XWebElementList([password.set_value(subscribe["password"])]))
-            password.web_element().send_keys(Keys.ENTER)
+        password = i_web_browser_operator.find_by_id(subscribe["pass_elem"])
+        i_web_browser_operator.send_value(
+            XWebElementList([password.set_value(subscribe["password"])])
+        )
+        password.web_element().send_keys(Keys.ENTER)
 
-        case SiteType.NEEDS_LOGIN_AND_ONE_STEP:
+    elif SiteType(subscribe["site_type"]) == SiteType.NEEDS_LOGIN_AND_ONE_STEP:
 
-            xbrowser = xbrowser_factory.create(xdriver, XUrl(subscribe["url"]))
-            i_web_browser_operator.boot(xbrowser)
-            
-            sleep(1)
-            user_id = i_web_browser_operator.find_by_id(subscribe["user_id_elem"])
-            
-            i_web_browser_operator.send_value(
-                XWebElementList(
-                    [
-                        user_id.set_value(subscribe["user_id"]),
-                    ]
-                )
+        xbrowser = xbrowser_factory.create(xdriver, XUrl(subscribe["url"]))
+        i_web_browser_operator.boot(xbrowser)
+
+        sleep(1)
+        user_id = i_web_browser_operator.find_by_id(subscribe["user_id_elem"])
+
+        i_web_browser_operator.send_value(
+            XWebElementList(
+                [
+                    user_id.set_value(subscribe["user_id"]),
+                ]
             )
+        )
 
+        password = i_web_browser_operator.find_by_id(subscribe["pass_elem"])
+        i_web_browser_operator.send_value(
+            XWebElementList([password.set_value(subscribe["password"])])
+        )
+        # password.web_element().send_keys(Keys.ENTER)
 
-            password = i_web_browser_operator.find_by_id(subscribe["pass_elem"])
-            i_web_browser_operator.send_value(XWebElementList([password.set_value(subscribe["password"])]))
-            # password.web_element().send_keys(Keys.ENTER)
-
-            i_web_browser_operator.find_by_xpath(subscribe["submit_elem"]).web_element().click()
-            sleep(1)
-        case _:
-            pass
+        i_web_browser_operator.find_by_xpath(
+            subscribe["submit_elem"]
+        ).web_element().click()
+        sleep(1)
+    else:
+        pass

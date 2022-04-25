1a681c0a7eaff16db536d6f142186d71a46a4f84
nishigaki <nishigaki@aivick.co.jp>
2022-04-25 19:18:29 +0900

    fix soup

--- shared/Domain/Scraping/soup_factory.py
+++ shared/Domain/Scraping/soup_factory.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from bs4 import BeautifulSoup
 from shared.i_factory import IFactory
 from shared.Domain.xurl import XUrl
@@ -5,6 +6,15 @@ import requests
 
 
 class SoupFactory(IFactory):
-    def create(self, xurl: XUrl) -> BeautifulSoup:
-        res = requests.get(xurl.get_href())
-        return BeautifulSoup(res.text, "html.parser")
+    def create(self, xurl: XUrl, cookie: Dict = None) -> BeautifulSoup:
+
+        if cookie:
+            session = requests.session()
+            session.get(xurl.get_href())
+
+            return BeautifulSoup(
+                requests.get(xurl.get_href(), cookies=cookie).content, "html"
+            )
+        else:
+            res = requests.get(xurl.get_href())
+            return BeautifulSoup(res.text, "html.parser")

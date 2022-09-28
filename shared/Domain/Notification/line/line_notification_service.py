89a73cad9eca47cff7dabc7c76cf4a1aad214860
masayan1126 <masa199311266@gmail.com>
2022-09-28 19:08:11 +0900

    Fix

--- shared/Domain/Notification/line/line_notification_service.py
+++ shared/Domain/Notification/line/line_notification_service.py
@@ -2,18 +2,17 @@ from dataclasses import dataclass
 import requests
 
 from shared.Domain.Notification.notification import Notification
-from packages.today_task_notification.config import CONFIG
 
 
 @dataclass
 class LineNotificationService:
     notification: Notification
 
-    def send(self) -> int:
+    def send(self, LINE_NOTIFY_TOKEN) -> int:
         if self.notification.message() == "":
             self.notification.set_message("通知するメッセージがありませんでした")
 
-        headers = {"Authorization": f"Bearer {CONFIG['LINE_NOTIFY_TOKEN']}"}
+        headers = {"Authorization": f"Bearer {LINE_NOTIFY_TOKEN}"}
         data = {"message": f"{self.notification.message()}"}
         res = requests.post(
             self.notification.destination_url(), headers=headers, data=data

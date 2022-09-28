53f03593a02818819b75754dd4060df080b7be94
masayan1126 <masa199311266@gmail.com>
2022-09-28 13:11:49 +0900

    Fix notify logic and add test

--- shared/Domain/Notification/line/line_notification_service.py
+++ shared/Domain/Notification/line/line_notification_service.py
@@ -1,15 +1,19 @@
+from dataclasses import dataclass
 import requests
 
 from shared.Domain.Notification.notification import Notification
+from packages.today_task_notification.config import CONFIG
 
 
+@dataclass
 class LineNotificationService:
-    def __init__(self, notification: Notification):
-        self.notification = notification
+    notification: Notification
 
     def send(self) -> int:
+        if self.notification.message() == "":
+            self.notification.set_message("通知するメッセージがありませんでした")
 
-        headers = {"Authorization": f"Bearer {self.notification.token()}"}
+        headers = {"Authorization": f"Bearer {CONFIG['LINE_NOTIFY_TOKEN']}"}
         data = {"message": f"{self.notification.message()}"}
         res = requests.post(
             self.notification.destination_url(), headers=headers, data=data
---
+++ tests/Domain/Notification/line/test_line_notification_service.py
@@ -0,0 +1,20 @@
+from unittest.mock import Mock, patch
+import pytest
+from shared.Domain.Notification.line.line_notification_service import (
+    LineNotificationService,
+)
+from shared.Domain.Notification.notification import Notification
+
+
+@patch("shared.Domain.Notification.line.line_notification_service.requests")
+def test_LINEに通知を送信できる(mock_requests):
+    mock_response = Mock(status_code=200)
+    mock_requests.post.return_value = mock_response
+
+    line_notification_service = LineNotificationService(
+        Notification("https://hoge/api/notify", "通知メッセージです")
+    )
+
+    expected = 200
+    actual = line_notification_service.send()
+    assert expected == actual

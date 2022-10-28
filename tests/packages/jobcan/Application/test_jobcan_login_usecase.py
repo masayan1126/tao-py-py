2fa62e9fb5530668e347aa889666a345bde11e73
masayan1126 <masa199311266@gmail.com>
2022-10-28 19:49:13 +0900

    Add mock env

--- tests/packages/jobcan/Application/test_jobcan_login_usecase.py
+++ tests/packages/jobcan/Application/test_jobcan_login_usecase.py
@@ -1,4 +1,20 @@
 from unittest.mock import MagicMock, patch
+
+
+# モジュールをモックへ差し替えるように追加(Gitリポジトリに存在しないconfigなど)
+import sys
+
+env_mock = MagicMock()
+# config_mock.CONFIG = {
+#     "CONSUMER_KEY": "CONSUMER_KEY1",
+#     "CONSUMER_SECRET": "CONSUMER_SECRET1",
+#     "ACCESS_TOKEN": "ACCESS_TOKEN1",
+#     "ACCESS_SECRET": "ACCESS_SECRET1",
+# }
+sys.modules["packages.jobcan.Application.jobcan_login_usecase.ENV"] = env_mock
+
+
+# memo: テスト対象のクラスでconfigを使用しているので、mockした後にimportする必要あり
 from packages.jobcan.Application.jobcan_login_usecase import JobcanLoginUsecase
 
 

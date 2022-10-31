c0323c94a64cf6eda89fafbde758d2d675065af3
masayan1126 <masa199311266@gmail.com>
2022-10-31 20:14:38 +0900

    Fix Fix/browser-operator-logic and test

--- shared/Core/di_container.py
+++ shared/Core/di_container.py
@@ -2,15 +2,12 @@ import injector
 from shared.Domain.Scraping.html_analyzer_impl import HtmlAnalyzerImpl
 from shared.Domain.Scraping.html_analyzer import HtmlAnalyzer
 
-from shared.Domain.Scraping.web_browser_operator import WebBrowserOperator
-from shared.Domain.Scraping.web_browser_operator_impl import WebBrowserOperatorImpl
-
 
 class DiContainer:
     def mappings(self, binder) -> None:
-        binder.bind(
-            WebBrowserOperator, to=injector.InstanceProvider(WebBrowserOperatorImpl())
-        )
+        # binder.bind(
+        #     WebBrowserOperator, to=injector.InstanceProvider(WebBrowserOperatorImpl())
+        # )
         binder.bind(HtmlAnalyzer, to=injector.InstanceProvider(HtmlAnalyzerImpl()))
 
         # TODO: wp,textfile,twi,automatic,calendar,SoftWareProcessOperator
--- shared/Domain/Scraping/browser_type.py
+++ shared/Domain/Scraping/browser_type.py
@@ -4,3 +4,5 @@ from enum import Enum
 class BrowserType(Enum):
     CHROME = 1
     FIREFOX = 2
+    EDGE = 3
+    SAFARI = 4
